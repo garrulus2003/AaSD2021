e5d0a6b2658aeb4127f45d427c004712458a4edf
garrulus2003 <54739772+garrulus2003@users.noreply.github.com>
2021-10-30 18:08:59 +0300

    Update 1D.cpp
    Исправила кодстайл, сделала бор классом, в котором разобралась с приватностью, отдельные функции вынесла из main.
    Посылка: https://codeforces.com/group/R3IJoiTue4/contest/344133/submission/133642301

--- 1D.cpp
+++ 1D.cpp
@@ -1,99 +1,131 @@
 //Words sorting realization via Trie structure
 
-
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
-#include <deque>
 
-struct trie{
-    struct Node{
+class trie {
+private:
+    struct Node {
         int terminal;
         std::map<char, int> to;
         Node() = default;
     };
-    std::vector<Node> t;
-    void add(std::string s);
-    std::vector<std::string> answer();
-    void dfs(int i, std::vector<std::string>& ans, std::string& s);
-    trie() {
-        t.resize(1);
+
+    std::vector<Node> nodes;
+    void add(const std::string& s);
+    void all_words(int i, std::vector<std::string>& ans, std::string& s);
+
+public:
+    std::vector<std::string> all_words();
+    trie() { nodes.resize(1); }
+    trie(const std::vector<std::string>& v)
+    {
+        nodes.resize(1);
+        for (const auto& str : v)
+            add(str);
     }
 };
 
-void trie::add(std::string s) {
+void trie::add(const std::string& s)
+{
     int current = 0;
     int index = 0;
-    while (index < s.size() && t[current].to[s[index]]){
-        current = t[current].to[s[index]];
+    while (index < s.size() && nodes[current].to[s[index]]) {
+        current = nodes[current].to[s[index]];
         ++index;
     }
-    while (index < s.size()){
-        t.push_back(Node());
-        t[current].to[s[index]] = t.size() - 1;
-        current = t.size() - 1;
+    while (index < s.size()) {
+        nodes.push_back(Node());
+        nodes[current].to[s[index]] = nodes.size() - 1;
+        current = nodes.size() - 1;
         ++index;
     }
-    t[current].terminal += 1;
+    nodes[current].terminal += 1;
 }
 
-void trie::dfs(int i,std::vector<std::string>& ans, std::string& s){
-    for (int j = 0; j < t[i].terminal; ++j){
+void trie::all_words(int i, std::vector<std::string>& ans, std::string& s)
+{
+    for (int j = 0; j < nodes[i].terminal; ++j) {
         ans.push_back(s);
     }
-    for (auto item : t[i].to){
-        dfs(item.second, ans, s += item.first);
+    for (auto item : nodes[i].to) {
+        all_words(item.second, ans, s += item.first);
     }
     s.pop_back();
 }
 
-
-std::vector<std::string> trie::answer(){
+std::vector<std::string> trie::all_words()
+{
     std::vector<std::string> ans;
     std::string s = "";
-    dfs(0, ans, s);
+    all_words(0, ans, s);
     return ans;
 }
 
-int main(){
-    trie t;
-    std::string s;
-    std::cin >> s;
-    std::vector<std::string> ss;
+std::vector<int> count_skips(const std::string& s)
+{
     std::vector<int> skips;
-    for (int i = 0; i < s.size(); ++i){
-        if (s[i] == '.'){
-            if (i == 0 || s[i - 1] != '.') skips.push_back(0);
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '.') {
+            if (i == 0 || s[i - 1] != '.')
+                skips.push_back(0);
             ++skips[skips.size() - 1];
             continue;
         }
-        if (i == 0 || s[i - 1] == '.') ss.push_back("");
-        ss[ss.size() - 1] += s[i];
     }
-    for (int i = 0; i < ss.size(); ++i) t.add(ss[i]);
-    std::vector<std::string> ans = t.answer();
-    if (s[0] == '.') {
-        for (int i = 0; i < ans.size(); ++i){
-            for (int j = 0; j < skips[i]; ++j){
-                std::cout << ".";
-            }
-            std::cout << ans[i];
-        }
-        if (ans.size() < skips.size()) {
-            for (int j = 0; j < skips[skips.size() - 1]; ++j){
-                std::cout << ".";
-            }
-        }
-        return 0;
+    return skips;
+}
+
+std::vector<std::string> find_all_words(const std::string& s)
+{
+    std::vector<std::string> wordlist;
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '.')
+            continue;
+        if (i == 0 || s[i - 1] == '.')
+            wordlist.push_back("");
+        wordlist[wordlist.size() - 1] += s[i];
     }
-    for (int i = 0; i < skips.size(); ++i){
-        std::cout << ans[i];
-        for (int j = 0; j < skips[i]; ++j){
-            std::cout << ".";
-        }
+    return wordlist;
+}
+
+template <typename T, typename U>
+void print_merged(const std::vector<T>& first, const std::vector<U>& second)
+{
+    for (size_t i = 0; i < second.size(); ++i) {
+        std::cout << first[i] << second[i];
     }
-    if (ans.size() > skips.size()) std::cout << ans[ans.size() - 1];
+    if (second.size() < first.size())
+        std::cout << first.back();
+}
+
+std::vector<std::string> skips_to_points(std::vector<int>& skips)
+{
+    std::vector<std::string> points;
+    for (auto skip : skips) {
+        points.push_back("");
+        for (int i = 0; i < skip; ++i)
+            points.back() += ".";
+    }
+    return points;
+}
+int main()
+{
+    std::string s;
+    std::cin >> s;
+
+    std::vector<std::string> wordlist = find_all_words(s);
+    trie t(wordlist);
+    std::vector<std::string> sorted_wordlist = t.all_words();
+
+    std::vector<int> skips = count_skips(s);
+    std::vector<std::string> points = skips_to_points(skips);
+
+    if (s[0] == '.')
+        print_merged<std::string, std::string>(points, sorted_wordlist);
+    else
+        print_merged<std::string, std::string>(sorted_wordlist, points);
     return 0;
 }
